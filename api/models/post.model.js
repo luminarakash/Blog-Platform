1047ab917902710739052ad5e8151b87c2799488
Akash Patel <117297200+luminarakash@users.noreply.github.com>
2024-11-14 15:13:06 +0530

    Add files via upload

---
+++ api/models/post.model.js
@@ -0,0 +1,38 @@
+import mongoose from 'mongoose';
+
+const postSchema = new mongoose.Schema(
+  {
+    userId: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      required: true,
+    },
+    title: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    image: {
+      type: String,
+      default:
+        'https://i.pinimg.com/736x/ab/3d/1f/ab3d1f9f8112aa100e0c9765c5c0008d.jpg',
+    },
+    category: {
+      type: String,
+      default: 'uncategorized',
+    },
+    slug: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const Post = mongoose.model('Post', postSchema);
+
+export default Post;
---
+++ api/models/user.model.js
@@ -0,0 +1,34 @@
+import mongoose from 'mongoose';
+
+const userSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    profilePicture: {
+      type: String,
+      default:
+        'https://i.pinimg.com/564x/67/73/9d/67739d5dfd3eca80dd7a934872b515bf.jpg',
+    },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model('User', userSchema);
+
+export default User;
